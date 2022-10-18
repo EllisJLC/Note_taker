5e82ac7c7cbb037492f5afddfafc5c6f950e255c
EllisJLC <jia.liang.chen@ryerson.ca>
2022-10-17 21:43:41 -0400

    final edits

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -1,24 +1,5 @@
 const fs = require('fs');
-const util = require('util');
 
-// Promise version of fs.readFile
-const readFromFile = util.promisify(fs.readFile);
-/**
- *  Function to write data to the JSON file given a destination and some content
- *  @param {string} destination The file you want to write to.
- *  @param {object} content The content you want to write to the file.
- *  @returns {void} Nothing
- */
-const writeToFile = (destination, content) =>
-  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
-    err ? console.error(err) : console.info(`\nData written to ${destination}`)
-  );
-/**
- *  Function to read data from a given a file and append some content
- *  @param {object} content The content you want to append to the file.
- *  @param {string} file The path to the file you want to save to.
- *  @returns {void} Nothing
- */
 const readAndAppend = (content, file) => {
   fs.readFile(file, 'utf8', (err, data) => {
     if (err) {
@@ -26,9 +7,21 @@ const readAndAppend = (content, file) => {
     } else {
       const parsedData = JSON.parse(data);
       parsedData.push(content);
-      writeToFile(file, parsedData);
+      fs.writeFile(file, JSON.stringify(parsedData), (error) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("saved");
+        }
+      });
     }
   });
 };
 
-module.exports = { readFromFile, writeToFile, readAndAppend };
+const idGenerator = () =>
+  Math.floor((1 + Math.random()) * 0x10000)
+  .toString(16)
+  .substring(1);
+
+
+module.exports = { idGenerator, readAndAppend};
