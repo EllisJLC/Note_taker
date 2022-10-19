415b88baaf3d37542fedc278f8aec10065d0c731
EllisJLC <jia.liang.chen@ryerson.ca>
2022-10-19 09:25:44 -0400

    test

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require("express");
 const PORT = process.env.PORT || 8000;
 const app = express();
 const db_route = require("./routes/index");
-const path = require("path")
+const path = require("path");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => 
+app.get("/", (req, res) => //test
 res.sendFile(path.join(__dirname, "./public/index.html"))
 );
 app.get("/notes", (req, res) => {
